da3f36521067871e37c905f13cd301f21c7b6950
Vincent Leclerc <vleclerc@inow.fr>
2026-04-20 17:09:42 +0200

    lazy loading

--- formation/src/app/app.routes.ts
+++ formation/src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
-import { TodoList } from './pages/todo/todo-list/todo-list';
-import { TodoAdd } from './pages/todo/todo-add/todo-add';
+/*import { TodoList } from './pages/todo/todo-list/todo-list';
+import { TodoAdd } from './pages/todo/todo-add/todo-add';*/
 import { Home } from './pages/home/home';
 import { NotFound } from './pages/not-found/not-found';
 import { Login } from './pages/authentication/login/login';
-import { TodoModule } from './pages/todo/todo-module';
+//import { TodoModule } from './pages/todo/todo-module';
 
 export const routes: Routes = [
     { path: '', component: Home },
     /*{ path: 'todos', component: TodoList },
     { path: 'todos/add', component: TodoAdd },*/
-    { path: 'todos', loadChildren: () => TodoModule },
+    { path: 'todos', loadChildren: () => import('./pages/todo/todo-module').then(m => m.TodoModule) },
     { path: 'auth/login', component: Login },
     { path: '404', component: NotFound },
     { path: '**', redirectTo: '404' } //derniere route, doit être à la fin, redirige vers la page 404
