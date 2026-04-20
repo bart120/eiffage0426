b6bd7e94994250af382ecf6a7bdd4807d75deaba
Vincent Leclerc <vleclerc@inow.fr>
2026-04-20 14:36:24 +0200

    menu

--- formation/src/app/shared/menu/menu.component.ts
+++ formation/src/app/shared/menu/menu.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeModule } from '../../core/prime.module';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
     standalone: true,
-    imports: [PrimeModule]
+    imports: [PrimeModule, MenubarModule]
 })
 
 export class MenuComponent implements OnInit {
+
+    items: MenuItem[] = [
+        {
+            label: 'ACCUEIL',
+            icon: 'pi pi-home'
+        },
+        {
+            label: 'A PROPOS',
+            icon: 'pi pi-info'
+        }
+
+    ];
+
     constructor() { }
 
     ngOnInit() { }
