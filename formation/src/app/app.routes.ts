186683ff5c454c82aedc194e7e1622bf3fb24d9f
Vincent Leclerc <vleclerc@inow.fr>
2026-04-21 09:34:38 +0200

    lazyloading component

--- formation/src/app/app.routes.ts
+++ formation/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 /*import { TodoList } from './pages/todo/todo-list/todo-list';
 import { TodoAdd } from './pages/todo/todo-add/todo-add';*/
-import { Home } from './pages/home/home';
+//import { Home } from './pages/home/home';
 import { NotFound } from './pages/not-found/not-found';
 //import { Login } from './pages/authentication/login/login';
 //import { TodoModule } from './pages/todo/todo-module';
 
 export const routes: Routes = [
-    { path: '', component: Home },
+    { path: '', loadComponent: () => import('./pages/home/home').then(m => m.Home) },
     /*{ path: 'todos', component: TodoList },
     { path: 'todos/add', component: TodoAdd },*/
     { path: 'todos', loadChildren: () => import('./pages/todo/todo-module').then(m => m.TodoModule) },
