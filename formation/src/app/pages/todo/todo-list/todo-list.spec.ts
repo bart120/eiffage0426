969c7e4bcb5c0affc81c05b82da3e4ab38871889
Vincent Leclerc <vleclerc@inow.fr>
2026-04-22 17:16:23 +0200

    tests

--- formation/src/app/pages/todo/todo-list/todo-list.spec.ts
+++ formation/src/app/pages/todo/todo-list/todo-list.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TodoList } from './todo-list';
+import { SelectModule } from 'primeng/select';
+import { PrimeModule } from '../../../core/prime.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 describe('TodoList', () => {
   let component: TodoList;
@@ -8,7 +12,10 @@ describe('TodoList', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [CommonModule, PrimeModule,
+        FormsModule, ReactiveFormsModule],
       declarations: [TodoList],
+      providers: []
     }).compileComponents();
 
     fixture = TestBed.createComponent(TodoList);
